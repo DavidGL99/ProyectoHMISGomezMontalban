0542c1a8891752e4eceb10251ac086696b098f76
GUILLERMO MONTALBAN MARTINEZ <guillermosysti@gmail.com>
2021-05-06 17:13:41 +0200

    codigo del proyecto ejercico project euler 1

--- ProyectoHMIS2021AzurePipeline/src/ual/hmis/ejercicio/MultiplesOf3And5.java
+++ ProyectoHMIS2021AzurePipeline/src/ual/hmis/ejercicio/MultiplesOf3And5.java
@@ -1,5 +1,17 @@
 package ual.hmis.ejercicio;
 
-public class MultiplesOf3And5 {
+import java.util.ArrayList;
 
+
+public class MultiplesOf3And5 {
+	
+	public static ArrayList<Integer> multiplesOf3And5 (int numeroLimite) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for (int i = 3; i < numeroLimite; i++) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				result.add(i);
+			}
+		}
+		return result;
+	}
 }
